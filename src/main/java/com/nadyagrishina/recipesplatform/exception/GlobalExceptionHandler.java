30dd469d17fb280a9ea942a463b0d2994f27aaa2
nadyagrishina <=>
2025-12-11 22:40:46 +0100

    feat: add exception handler + custom exceptions

--- src/main/java/com/nadyagrishina/recipesplatform/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nadyagrishina/recipesplatform/exception/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.nadyagrishina.recipesplatform.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> handleConflictException(ConflictException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
