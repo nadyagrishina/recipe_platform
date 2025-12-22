6026b17671b680a6737e27996f1493766321b3f2
Nadya Grishina <nadezhda.grishina2002@gmail.com>
2025-12-22 17:38:40 +0300

    add basic frontend files

---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,5 @@
+export const ROUTES = {
+  HOME: "/",
+  RECIPES: "/recipes",
+  CREATE_RECIPE: "/recipes/new",
+};
---
+++ frontend/src/constants/texts.ts
@@ -0,0 +1,8 @@
+export const TEXTS = {
+  header: {
+    logo_subtext: "online cookbook",
+    login_button: "Login",
+    language_cz: "CZ",
+    language_en: "EN",
+  },
+};
